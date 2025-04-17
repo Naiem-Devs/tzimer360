88c6f0af1bc119d927c295ccde50da3316c9b179
Naiem-Devs <dnsnaiem@gmail.com>
2025-04-17 23:10:33 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+ // menu 
+//  $('.siteBar-btn').click( function (event){ 
+//     event.preventDefault()
+//     $(this).toggleClass('active');   
+//     $('.mobile-menu').toggleClass('siteBar');   
+//   }); 
+
+var swiper = new Swiper(".place-card-swiper", {
+  navigation: {
+    nextEl: ".swiper-button-right",
+    prevEl: ".swiper-button-left",
+  },
+});
+
+
+
+  // page Animation
+  // AOS.init({
+  //   mirror: true,
+  //   duration: 1500,
+  //   initClassName: 'aos-init',
+  //   once: true,
+  // });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="300" 
+
+ 
